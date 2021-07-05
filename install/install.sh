2245116367a27a50f6cf821a40c457c01df25675
TamaGoes <tamatama@tensi.org>
2021-07-05 22:37:42 +0900

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+sudo apt update -y
+
+# Planning to use ‘yad’ gui.
+# Install ‘yad’ if it’s not installed.
+if [ -v $(which yad) ]
+  sudo apt install -y yad
+fi
+
+# Some CPU tools use cpulimit.
+# Install ‘cpulimit’ if it’s not installed.
+if [ -v $(which yad) ]
+  sudo apt install -y yad
+fi
